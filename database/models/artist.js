14cf40c160b967be8e20e87bbaac94f763f44946
Seungjae Moon <sjaymoon15@gmail.com>
2016-12-08 12:58:17 -0500

    add album and artist schemas

--- database/models/artist.js
+++ database/models/artist.js
@@ -1,1 +1,22 @@
-// Todo: Create Artist Model
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const AlbumSchema = require('./album');
+
+const ArtistSchema = new Schema({
+	name: {
+		type: String,
+		required: [true, 'Name is required.']
+	},
+	age: Number,
+	yearsActive: Number,
+	image: String,
+	genre: String,
+	website: String,
+	netWorth: Number,
+	labelName: String,
+	retired: Boolean,
+	Albums: [AlbumSchema]
+});
+
+const Artist = mongoose.model('artist', ArtistSchema);
+mondule.exports = Artist;
